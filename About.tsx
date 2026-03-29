90f19cc140f3b6105fac568677f7b0381152cb4a
jaysah9999 <jaykrsah23@gmail.com>
2026-03-29 12:17:18 +0530

    add all files

---
+++ About.tsx
@@ -0,0 +1,16 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          I am a engineer with a passion for building innovative and scalable web applications. I have experience in full stack development and am always looking to learn new technologies and improve my skills.
+
+      </div>
+    </div>
+  );
+};
+
+export default About;
